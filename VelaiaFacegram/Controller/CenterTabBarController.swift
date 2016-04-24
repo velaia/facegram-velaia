577326710531d276b63fadf69b6d7b66f320a6a7
Daniel N. Lang <daniel.lang@velaia.de>
2016-04-24 16:56:07 +0200

    Creating Image Slideup Animation. Didn't change much for me in the simulator UI - no slide-up animation ...

---
+++ VelaiaFacegram/Controller/CenterTabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  CenterTabBarController.swift
+//  VelaiaFacegram
+//
+//  Created by Daniel N Lang on 24.04.16.
+//  Copyright © 2016 Daniel N Lang. All rights reserved.
+//
+
+import UIKit
+
+class CenterTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let centerButton = UIButton(type: .Custom)
+        let buttonImage = UIImage(named: "camera")
+        let numTabs = self.viewControllers!.count
+        
+        if buttonImage != nil {
+            let screenWidth = UIScreen.mainScreen().bounds.size.width
+            centerButton.frame = CGRectMake(0, 0, screenWidth / CGFloat(numTabs), self.tabBar.frame.size.height)
+            centerButton.setImage(buttonImage, forState: .Normal)
+            centerButton.tintColor = UIColor.whiteColor()
+            centerButton.backgroundColor = UIColor(red: 18/255.0, green: 86/255.0, blue: 136/255.0, alpha: 1.0)
+            
+            centerButton.center = self.tabBar.center
+            
+            centerButton.addTarget(self, action: #selector(CenterTabBarController.showCamera(_:)), forControlEvents: .TouchUpInside)
+            self.view.addSubview(centerButton)
+        }
+    }
+    
+    func showCamera(sender: UIButton!) {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+        let cameraPicker = mainStoryboard.instantiateViewControllerWithIdentifier("CameraPopup")
+        self.presentViewController(cameraPicker, animated: true, completion: nil)
+    }
+}
